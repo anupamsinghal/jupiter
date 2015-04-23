63c6e71e9ffaf595cde74039a2244d73b9cf3e8b
Anupam Singhal <anupam.singhal@gmail.com>
2015-04-22 21:55:14 -0700

    works!

---
+++ README.md
@@ -0,0 +1,34 @@
+# README #
+
+Merges subtitles from 2 files to allow both languages to show up.  For someone learning a new language.
+
+ver 1: based on counter. Works but hard to find 2 subtitles that match up well.
+ver 2: based on timestamp.  Works well.
+
+
+### What is this repository for? ###
+
+* Quick summary
+Code for project that are private for now but I may want to share later.
+
+* Version
+2.0
+
+* [Learn Markdown](https://bitbucket.org/tutorials/markdowndemo)
+
+### How do I get set up? ###
+
+* Summary of set up
+python 2.7.3+
+
+
+### Contribution guidelines ###
+
+* Writing tests
+* Code review
+* Other guidelines
+
+### Who do I talk to? ###
+
+* Repo owner or admin
+Anupam Singhal
--- merge_subtitles.py
+++ merge_subtitles.py
@@ -22,6 +22,10 @@ import urllib
 import os
 import os.path
 from utils import *
+from time import strptime  # parse time
+from time import strftime  # format time
+from time import mktime
+
 
 def parse_args():
    """Parses arguments"""
@@ -33,11 +37,12 @@ def parse_args():
    p.add_option("-1", dest="first", help="first file (default: %default)")
    p.add_option("-2", dest="second", help="second file (default: %default)")
    p.add_option("-o", dest="output", help="output merged file (default: %default)")
+   p.add_option("--use_ts", action="store_true", dest="use_ts", help="use timestamp, else counter (default: %default)")   
    p.add_option("--overwrite", action="store_true", dest="overwrite", help="overwrite file if exists (default: %default)")   
    p.add_option("-d", "--diff", dest="diff", help="if 0, same numbers are merged. If 2, second:3 will get merged to first:1. If -2, second:3 will get merged to first:5 (default: %default)")
 
    # set defaults
-   p.set_defaults(loglevel="info", diff = 0, first="1.srt", second="2.srt", output="3.srt", overwrite=True)
+   p.set_defaults(loglevel="info", diff = 0, first="1.srt", second="2.srt", output="3.srt", use_ts = False, overwrite=True)
    (opts, args) = p.parse_args()
 
    opts.diff = int(opts.diff)
@@ -45,8 +50,135 @@ def parse_args():
    return opts
 
 
-def main():
+class TS:
+   def __init__(self, ts, ms):
+      self.ts = strptime("2000 " + ts, "%Y %H:%M:%S")  # 2000 is to make time.mktime() work properly
+      self.ms = ms
 
+   def get_ts(self):
+      return strftime("%H:%M:%S", self.ts)
+
+   def __str__(self):
+     return "%s,%d" % (self.get_ts(), self.ms)
+
+   def __sub__(self, other):
+      a = mktime(self.ts) + float(self.ms) / 1000
+      b = mktime(other.ts) + float(other.ms) / 1000
+      debug("returning: %f" % (a-b))
+      return a - b
+
+
+def parse_from_to(ts):
+   tokens = ts.split(' ')
+   start = tokens[0].split(',')
+   start = TS(start[0], int(start[1]))
+
+   end = tokens[2].split(',')
+   end = TS(end[0], int(end[1]))
+
+   debug("%s %d, %s %d" % (start.get_ts(), start.ms, end.get_ts(), end.ms))
+   return (start, end)
+
+
+def parse_ts(lines):
+   ptr = 0
+   items = list()
+   while ptr < len(lines):
+      # skip blank line
+      if len(lines[ptr].strip()) == 0:
+         ptr += 1
+
+      # discard counter
+      ptr += 1
+
+      if ptr >= len(lines):
+         break
+
+      # read ts
+      ts = parse_from_to(lines[ptr].strip())
+      ptr += 1
+
+      text = ""
+      # read text until blank line
+      while len(lines[ptr].strip()) != 0:
+         text += lines[ptr]
+         ptr += 1
+         debug("incr ptr: %d" % ptr)
+
+      text = text.strip()
+      debug("text: %s" % text)
+      # append to items
+      tuple = (ts, text)
+      items.append(tuple)
+
+   return items
+
+
+
+def ts_write(out, counter, start, end, text):
+   out.write("%d\n" % counter)
+   out.write("%s --> %s\n" % (start, end))
+   out.write(text + "\n")
+   out.write("\n")
+
+
+def do_ts(opts, out, lines1, lines2):
+   # remove counters
+   # parse into array of tuples (from, to, list of lines)
+   lang1 = parse_ts(lines1)   
+   lang2 = parse_ts(lines2)
+   trace("1: %d, 2: %d" % (len(lang1), len(lang2)))
+
+   # init
+   counter = 1
+   m_counter = s_counter = 0
+
+   # cases for merging language text
+   # A: merge text from 1 and 2
+   # B: output text from 1 only
+   # C: output text from 2 only
+
+   # cases for timestamp merge - avoid overlap
+   # 1: determine start: use from whatever lang has more counters
+   # 2: determine end:  use from whatever lang has more counters
+   # 3: determine merge case:
+   #     if start2 - start1 <= 1 second  --> merge
+   #        else                         --> more counter lang only
+
+   if len(lang1) >= len(lang2):
+      master = lang1
+      slave = lang2
+   else:
+      master = lang2
+      slave = lang1
+
+   # loop
+   while counter <= len(master):
+      debug("*** looping: %d" % counter)
+
+      (m_ts, m_text) = master[m_counter]
+      (m_start, m_end) = m_ts
+
+      (s_ts, s_text) = slave[s_counter]
+      (s_start, s_end) = s_ts
+
+      if s_start - m_start <= 1.0 and s_counter < len(slave):
+         # merge the two languages
+         debug("merge")
+         text = m_text + "\n" + s_text
+         m_counter += 1
+         s_counter += 1
+      else:
+         # emit master only
+         debug("master only")
+         text = m_text
+         m_counter += 1
+
+      ts_write(out, counter, m_start, m_end, text)
+      counter += 1
+
+
+def main():
    opts = parse_args()
    log = init_logging(opts.loglevel, opts.logfile)
 
@@ -69,6 +201,17 @@ def main():
 
    out = open(opts.output, 'a')
 
+   if opts.use_ts:
+      do_ts(opts, out, lines1, lines2)
+   else:
+      do_counter(opts, out, lines1, lines2)
+
+   out.close()
+
+
+
+def do_counter(opts, out, lines1, lines2):
+
    # what counter we are on
    counter1 = int(lines1[0].strip().decode('utf-8-sig'))
    counter2 = int(lines2[0].strip().decode('utf-8-sig'))
@@ -76,9 +219,6 @@ def main():
    # line number in file
    ptr1 = 0
    ptr2 = 0
-
-   trace("first: starts at %d" % counter1)
-   trace("second: starts at %d" % counter2)
 
    # have we reached end of file?
    done1 = False
@@ -101,13 +241,13 @@ def main():
             ptr1 += 1
 
 
-   # loop over each line in first file
+   # begin the process
    while  ptr1 < len(lines1):
-      if len(lines1[ptr1].strip()) > 0:
+      if ptr1 < len(lines1) and len(lines1[ptr1].strip()) > 0:
          counter1 = int(lines1[ptr1].strip().decode('utf-8-sig'))
       else:
          done1 = True
-      if len(lines2[ptr2].strip()) > 0:
+      if ptr2 < len(lines2) and len(lines2[ptr2].strip()) > 0:
          counter2 = int(lines2[ptr2].strip().decode('utf-8-sig'))
       else:
          done2 = True
@@ -141,11 +281,10 @@ def main():
          ptr2 += 1
       out.write("\n")
 
-   out.close()
-   #trace("#\t TOTAL TIME: " + get_elapsed_time( int(time.time() - start)))
 
 
 if __name__ == '__main__':
    main()
 else:
    pass
+
